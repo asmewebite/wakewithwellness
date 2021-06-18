228e5d73c5ed81337d723d0072fd83124bb1e59a
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-06-18 18:19:20 +0530

    Add files via upload

---
+++ feedback.js
@@ -0,0 +1,37 @@
+//Connection
+var firebaseConfig = {
+    apiKey: "AIzaSyBTOvD0aod-fntZ0VYbT6EAp9z1L_bqKwY",
+    authDomain: "website-818ed.firebaseapp.com",
+    projectId: "website-818ed",
+    storageBucket: "website-818ed.appspot.com",
+    messagingSenderId: "542030860652",
+    appId: "1:542030860652:web:ee0bef0509915aa1da2349",
+    measurementId: "G-2VWCQ9QR4S"
+  };
+
+//Init Firebase
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+
+
+//Start grabbing our DOM element
+const submitBtn = document.querySelector('#submit');
+
+let userMessage = document.querySelector("#userMessage");
+
+const db = firestore.collection("feedback");
+
+submitBtn.addEventListener('click', function(){
+    let userMessageInput = userMessage.value;
+
+    //Access the Database
+    db.doc().set({
+        message: userMessageInput
+    }).then(function(){
+        console.log("Data Saved");
+       
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+});
