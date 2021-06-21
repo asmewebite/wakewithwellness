27827fa15217325f6cedc4698df9057ecbc917d1
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-06-21 18:55:30 +0530

    Update admin.js

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,58 +1,45 @@
-// DOM elements
-const contactDataList = document.querySelector('.contactData');
-const loggedOutLinks = document.querySelectorAll('.logged-out');
-const loggedInLinks = document.querySelectorAll('.logged-in');
-const accountDetails = document.querySelector('.account-details');
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
 
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
 
-const setupUI = (user) => {
-    if (user) {
-      // account info
-    const html = `
-    <div>Logged in as ${user.email}</div>
-    `;
-  accountDetails.innerHTML = html;
-      // toggle user UI elements
-      loggedInLinks.forEach(item => item.style.display = 'block');
-      loggedOutLinks.forEach(item => item.style.display = 'none');
-    } else {
-      // clear account info
-      accountDetails.innerHTML = '';
-      // toggle user elements
-      loggedInLinks.forEach(item => item.style.display = 'none');
-      loggedOutLinks.forEach(item => item.style.display = 'block');
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "User : " + email_id;
+
     }
-  };
 
-// setup Data
-const setupcontactData = (data) => {
+  } else {
+    // No user is signed in.
 
- if(data.length){
-  let html = '<h6 class= "center-align">Contact Data</h6>';
-  data.forEach(doc => {
-    const contactdata = doc.data();
-    const li = `
-      <li>
-        <div class="collapsible-header grey lighten-4"> ${contactdata.email} </div>
-        <div class="collapsible-body white"> ${contactdata.message} </div>
-      </li>
-    `;
-    html += li;
-  });
-  contactDataList.innerHTML = html;
-}else {
-    contactDataList.innerHTML = '<h5 class="center-align">Official Logins only</h5>'
-}
-};
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
 
-
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
-
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-
-  var items = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(items);
-
+  }
 });
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
