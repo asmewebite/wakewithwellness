a29812ddbb3a6fec44ce4e3d34bedc2a0b6ee307
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-07-10 18:13:44 +0530

    Add files via upload

---
+++ scripts/membersfeed.js
@@ -0,0 +1,35 @@
+const Memberslist = document.querySelector('#members');
+
+function renderMembers(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let message = document.createElement('span');
+   
+  
+
+
+    li.setAttribute('data-id',doc.id);
+    name.textContent = doc.data().name;
+    message.textContent = doc.data().message;
+    
+
+  
+
+
+    li.appendChild(name);
+    li.appendChild(message);
+
+    
+  
+
+    Memberslist.appendChild(li);
+
+}
+
+
+db.collection('membermsg').get().then((snapshot)=>{
+    snapshot.docs.forEach(doc => {
+        renderMembers(doc);
+    })
+
+})
