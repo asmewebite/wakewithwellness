ac46d0b056db6b8c5c9970b3024fe201a3476d13
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-08-03 10:17:20 +0530

    Update firestore-db.js

--- memberslogin/scripts/firestore-db.js
+++ memberslogin/scripts/firestore-db.js
@@ -90,7 +90,7 @@ async function getuserInfoRealtime(userID){
                         <h6 style="color: #5793D1;font-weight:500;padding:5px;text-align: center;">Notifications</h6>
                        <div style="text-align:left;padding-left:5px;padding-right:5px">
                         <ul>
-                        <li>* August timeline suggestion <a href="../timelinesuggestions.html">Click</a></li>
+                        
 
                         
                         </ul>
