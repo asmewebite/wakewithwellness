0dc0eacaafdd1e972a042516d333e0f41e7ac87f
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-08-03 23:17:31 +0530

    Update firestore-db.js

--- memberslogin/scripts/firestore-db.js
+++ memberslogin/scripts/firestore-db.js
@@ -90,7 +90,7 @@ async function getuserInfoRealtime(userID){
                         <h6 style="color: #5793D1;font-weight:500;padding:5px;text-align: center;">Notifications</h6>
                        <div style="text-align:left;padding-left:5px;padding-right:5px">
                         <ul>
-                        
+                        <li>Attendence to be posted on 15th & 30th of every month</li>
 
                         
                         </ul>
