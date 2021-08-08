a7484abac555348b3f52fe44628f3d4360c139d5
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-08-08 14:57:19 +0530

    Update eventReg.js

--- eventReg.js
+++ eventReg.js
@@ -1,16 +1,39 @@
  // Your web app's Firebase configuration
  var firebaseConfig = {
-  apiKey: "AIzaSyBTOvD0aod-fntZ0VYbT6EAp9z1L_bqKwY",
-  authDomain: "website-818ed.firebaseapp.com",
-  databaseURL: "https://website-818ed-default-rtdb.firebaseio.com",
-  projectId: "website-818ed",
-  storageBucket: "website-818ed.appspot.com",
-  messagingSenderId: "542030860652",
-  appId: "1:542030860652:web:ee0bef0509915aa1da2349",
-  measurementId: "G-2VWCQ9QR4S"
+  apiKey: "AIzaSyAF1DAJ7WFv8-BLO6NuDavi2S4ZdINW3Jw",
+  authDomain: "wakewithwellness-fcdb9.firebaseapp.com",
+  databaseURL: "https://wakewithwellness-fcdb9-default-rtdb.firebaseio.com",
+  projectId: "wakewithwellness-fcdb9",
+  storageBucket: "wakewithwellness-fcdb9.appspot.com",
+  messagingSenderId: "809455887855",
+  appId: "1:809455887855:web:e756652111b39913b3c21a",
+  measurementId: "G-4SKG6CT2GC"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+//function to save file
+function uploadFile(){
+      
+  // Created a Storage Reference with root dir
+  var storageRef = firebase.storage('submissions').ref('url');
+  // Get the file from DOM
+  var file = document.getElementById("files").files[0];
+  console.log(file);
+  
+  //dynamically set reference to the file name
+  var thisRef = storageRef.child(file.name);
+
+  //put request upload file to firebase storage
+  thisRef.put(file).then(function(snapshot) {
+     alert("File Uploaded")
+     console.log('Uploaded a blob or file!');
+  });
+}
+
+
+
 
 // Reference messages collection
 var dataRef = firebase.database().ref('EventReg');
@@ -26,13 +49,14 @@ function submitForm(e){
   var name = getInputVal('name');
   var email = getInputVal('Email');
   var contact = getInputVal('Contact');
+  var image = getInputVal('files');
 
 
   
 
 
   // Save message
-  saveData(name, email, contact);
+  saveData(name, email, contact,image);
 
 
  // Show alert
@@ -54,11 +78,12 @@ function getInputVal(id){
 }
 
 // Save message to firebase
-function saveData(name, email, contact){
+function saveData(name, email, contact,image){
   var newDataRef = dataRef.push();
   newDataRef.set({
     name: name,
     email:email,
-    contact:contact
+    contact:contact,
+    image:image
   });
 }
