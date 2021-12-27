c50092962e8493eada7a6869d37a1cba4c77e384
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2021-12-27 20:42:47 +0530

    Update internattendance.js

--- web/ADMIN/scripts/internattendance.js
+++ web/ADMIN/scripts/internattendance.js
@@ -84,3 +84,89 @@ db.collection('attendance').get().then((snapshot)=>{
 })
 
 
+
+
+// 20/12/21-26/12/21
+
+const attendanceDec3 = document.querySelector('#Dec3');
+
+function renderDec3(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let dept = document.createElement('span');
+    let Dec3 = document.createElement('span');
+    let Dec3f = document.createElement('span');
+  
+
+
+    li.setAttribute('data-id',doc.id);
+    name.textContent = doc.data().name;
+    dept.textContent = doc.data().dept;
+    Dec3.textContent = doc.data().Dec3;
+    Dec3f.textContent = doc.data().Dec3f;
+  
+
+
+    li.appendChild(name);
+    li.appendChild(dept);
+    li.appendChild(Dec3);
+    li.appendChild(Dec3f);
+    
+  
+
+    attendanceDec3.appendChild(li);
+
+}
+
+
+db.collection('attendance').get().then((snapshot)=>{
+    snapshot.docs.forEach(doc => {
+        renderDec3(doc);
+    })
+
+})
+
+
+
+
+// 27/12/21-2/12/21
+
+const attendanceDec4 = document.querySelector('#Dec4');
+
+function renderDec4(doc){
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let dept = document.createElement('span');
+    let Dec4 = document.createElement('span');
+    let Dec4f = document.createElement('span');
+  
+
+
+    li.setAttribute('data-id',doc.id);
+    name.textContent = doc.data().name;
+    dept.textContent = doc.data().dept;
+    Dec4.textContent = doc.data().Dec4;
+    Dec4f.textContent = doc.data().Dec4f;
+  
+
+
+    li.appendChild(name);
+    li.appendChild(dept);
+    li.appendChild(Dec4);
+    li.appendChild(Dec4f);
+    
+  
+
+    attendanceDec4.appendChild(li);
+
+}
+
+
+db.collection('attendance').get().then((snapshot)=>{
+    snapshot.docs.forEach(doc => {
+        renderDec4(doc);
+    })
+
+})
+
+
