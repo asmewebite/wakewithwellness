6519adfd21a8cbfa203481586307dea0584a6be9
Ankur jyoti Dutta <73809650+ankirdutta910@users.noreply.github.com>
2022-01-04 00:14:07 +0530

    Add files via upload

---
+++ web/MembersPanel/idcard.js
@@ -0,0 +1,51 @@
+ // Reference messages collection
+         var dataRef = firebase.database().ref('IDreg');
+
+// Listen for form submit
+document.getElementById('idreg').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var Email = getInputVal('email');
+  var Contact = getInputVal('contact');
+  var Blood = getInputVal('blood');
+  var Address = getInputVal('address');
+
+
+  
+
+
+  // Save message
+  saveData(name, Email, Contact, Blood, Address);
+
+
+
+ // Clear form
+ document.getElementById('idreg').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveData(name, Email, Contact, Blood, Address){
+  var newDataRef = dataRef.push();
+  newDataRef.set({
+    name: name,
+    Email:Email,
+    Contact:Contact,
+    Blood:Blood,
+    Address:Address
+  
+    
+
+  });
+}
+
+
